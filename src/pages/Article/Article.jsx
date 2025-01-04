5e245b4dfbf917893e0c694e5ea6659896370b72
muhammadzulfans <muhammadzulfans932@gmail.com>
2025-01-04 14:02:51 +0700

    [progres]

--- src/pages/Article/Article.jsx
+++ src/pages/Article/Article.jsx
@@ -37,13 +37,12 @@ const Article = () => {
       <div className="w-full">
         <Link
           to="/article/add"
-          className="bg-button hover:bg-amber-600 border border-slate-200 text-white font-bold py-2 px-4 rounded-xl"
+          className="bg-green-500 hover:bg-green-700 border border-slate-200 text-white font-bold py-2 px-4 rounded-lg"
         >
           Add New
         </Link>
-
-        <div className="relative shadow rounded-lg mt-14 mx-10">
-          <table className="w-full text-sm text-left text-gray-500 ">
+        <div className="relative shadow rounded-lg mt-3">
+          <table className="w-full text-sm text-left text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-gray-100">
               <tr>
                 <th className="py-3 px-1 text-center">No</th>
@@ -66,11 +65,10 @@ const Article = () => {
                   </td>
                   <td className="py-3 px-6 font-medium text-gray-900">{article.title}</td>
                   <td className="py-3 px-6">{article.deskripsi}</td>
-
-                  <th className="py-3 px-5 border border-black text-center flex space-x-7">
+                  <td className="py-3 px-1 text-center">
                     <Link
                       to={`article/edit/${article.id}`}
-                      className="font-medium bg-blue-400 hover:bg-blue-500 px-5 py-1 rounded text-white "
+                      className="font-medium bg-blue-400 hover:bg-blue-500 px-3 py-1 rounded text-white mr-1"
                     >
                       Edit
                     </Link>
@@ -80,7 +78,7 @@ const Article = () => {
                     >
                       Delete
                     </button>
-                  </th>
+                  </td>
                 </tr>
               ))}
             </tbody>
